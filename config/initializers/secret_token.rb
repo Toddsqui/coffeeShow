540f2072748ad9aea7d21c0c634d6b96c895e84c
Todd Squitieri <todd.squitieri@alumni.newschool.edu>
2015-08-27 13:23:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeShow::Application.config.secret_key_base = '439e11160897592c4ccefd0de01960a369329206e37ba506ea3fc17e099f41b3bf34b06e1dc3b6c20e1ad4cc89921bafd65cc2d02c892de19708a28c426ab0f3'
